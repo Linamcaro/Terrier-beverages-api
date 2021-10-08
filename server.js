dda912a3dbd8e3db24988d939008376195a0698e
Linamcaro <lina.mcaro@gmail.com>
2021-10-08 17:58:30 -0500

    update file structure

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
-import Express from 'express';
+import Express, { application } from 'express';
 
 const app = Express();
+app.use(Express.json());
+
+app.get('/products',(req,res)=>{
+    // query la base de datos
+    res.send('info de los productos');
+ 
+} );
+
+app.post('/products/new', (req,res)=>{
+    //implementar codigo para crear productos en la base de datos
+    console.log("create product: ",req.body);
+    res.send('product created');
+
+});
 
 app.listen(5000,()=>{
     console.log('Escuchando puerto 5000');
-
-
-
-})
+}); 
